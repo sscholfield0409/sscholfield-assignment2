b6bbe5563049add1a81a0646e222c32c8648ae2b
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-05-28 22:00:54 -0500

    v2 commit

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -3,7 +3,10 @@ package com.coderscampus;
 public class Assignment2Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		//generate a random number
+		
+		//collect user input
 
 	}
 
