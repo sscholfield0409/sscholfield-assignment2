a5c182d4779316538d65e9f5ae04f1f2b1fe2d77
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-06-04 21:54:38 -0500

    updated output/tries
    Confirmed the amount of tries and proper outputs are good to go.

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -7,10 +7,6 @@ public class Assignment2Application {
 
 	public static void main(String[] args) {
 		
-		//generate a random number
-		
-		//collect user input
-		
 		Scanner scanner = new Scanner(System.in);
 		Random random = new Random();
 		
@@ -18,29 +14,27 @@ public class Assignment2Application {
 		int tries = 0;
 		
 		System.out.println("Guess a number between 1 and 100");
-		
 		int guessNumber = scanner.nextInt();
-		while (guessNumber != randomNumber || tries < 5) {
 		
+		while (tries < 5) {
 			if ( guessNumber < randomNumber) {
-				System.out.println("Please Pick a Higher Number");
+				System.out.println("Please Pick a Higher Number" + ": ");
 			} else {
-				System.out.println("Please Pick a Lower Number");
+				System.out.println("Please Pick a Lower Number" + ": ");
 			}
-			
-			if ( guessNumber != randomNumber && tries == 5) {
-				System.out.println("You Lose! The Number was: " + randomNumber + ".");
-			} else {
-				System.out.println("You Win!");
+			if (guessNumber == randomNumber) {
+                System.out.println("Congratulations! You guessed the secret number in " + tries + " tries.");
 			}
 			
 			guessNumber = scanner.nextInt();
+			tries ++;
 			
-			scanner.close();
-			
-
+			if ( tries == 5) {
+				System.out.println("You Lose! The Number was: " + randomNumber + ".");
 			}
-
+		
+			}
+		scanner.close();
+		
 		}
-
-	}
+	 }
