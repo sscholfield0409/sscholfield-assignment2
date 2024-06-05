e8b5daadabb2c192158471247e32126b449a69a8
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-06-04 19:30:11 -0500

    update

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -1,5 +1,8 @@
 package com.coderscampus;
 
+import java.util.Scanner;
+import java.util.Random;
+
 public class Assignment2Application {
 
 	public static void main(String[] args) {
@@ -7,7 +10,30 @@ public class Assignment2Application {
 		//generate a random number
 		
 		//collect user input
+		
+		Scanner scanner = new Scanner(System.in);
+		Random random = new Random();
+		
+		int randomNumber = random.nextInt(100) + 1;
+		int tries = 0;
+		
+		System.out.println("Guess a number between 1 and 100");
+		
+		int guessNumber = scanner.nextInt();
+		while (guessNumber != randomNumber || tries < 5) {
+			if ( guessNumber < randomNumber) {
+				System.out.println("Please Pick a Higher Number");
+			} else {
+				System.out.println("Please Pick a Lower Number");
+			} if (tries == 5) {
+	            System.out.println("You Lose. The number was: " + randomNumber + ".");
+	        }
+			guessNumber = scanner.nextInt();
+			
+			System.out.println("You Win!");
+			scanner.close();
 
-	}
+			}
 
+		}
 }
