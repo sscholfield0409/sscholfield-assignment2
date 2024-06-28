cbd325d4acc460bdbc0603eec8348933ad12420b
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-06-27 19:27:11 -0500

    Update Assignment2Application.java
    update before reboot

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -23,28 +23,33 @@ while (tries < 5) {
 			if ( guessNumber1 < randomNumber) {
 				System.out.println("Please Pick a Higher Number" + ": ");
 				
-			} else if ( guessNumber1 > randomNumber) {
+			}
+			else if ( guessNumber1 > randomNumber) {
 				System.out.println("Please Pick a Lower Number" + ": ");
-			} 
-			tries ++;
-			guessNumber1 = scanner.nextInt();
+
+			}  tries++;
 			
-			
+            guessNumber1 = scanner.nextInt(); 
+            	
 			if (guessNumber1 == randomNumber && tries <= 5) {
                 System.out.println("Congratulations! You guessed the secret number which is: " + randomNumber);
 			}
-			
+
 			if (guessNumber1 > 100 || guessNumber1 < 1) {
 				System.out.println("Invalid Entry: You did not choose a number between 1 and 100: Try again");
-			}  
-	
+				continue;
+			}
+			
 			if ( tries == 5 && guessNumber1 != randomNumber) {
 				System.out.println("You Lose! The Number was: " + randomNumber + ".");
+				break;
 			}
 		
 	
-		}
+		
 		
 		scanner.close();
+	
 }
+	}
 }
