2a040d77ed90244eeffc69d65a82274bdb88fae4
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-06-28 23:29:56 -0500

    Update Assignment2Application.java
    making more attempts to try and adjust invalid input attempts accuracy according to the rules of the assignment

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -22,31 +22,32 @@ while (tries < 5) {
 			
 			if ( guessNumber1 < randomNumber) {
 				System.out.println("Please Pick a Higher Number" + ": ");
-			}
-			else if ( guessNumber1 > randomNumber) {
+			} else if ( guessNumber1 > randomNumber) {
 				System.out.println("Please Pick a Lower Number" + ": ");
-				} 
+			}
 			
-			tries++;
 			guessNumber1 = scanner.nextInt();
-
-		 	
+			tries ++;
+			
 			if (guessNumber1 == randomNumber && tries <= 5) {
-				System.out.println("Congratulations! You guessed the secret number which is: " + randomNumber);
-				break;
-			} 
+                System.out.println("Congratulations! You guessed the secret number which is: " + randomNumber);
+                break;
+			}
 			
 			if (guessNumber1 > 100 || guessNumber1 < 1) {
 				System.out.println("Invalid Entry: You did not choose a number between 1 and 100: Try again");
 				continue;
 			} 
 			
+			guessNumber1 = scanner.nextInt();
+	
 			if ( tries == 5 && guessNumber1 != randomNumber) {
 				System.out.println("You Lose! The Number was: " + randomNumber + ".");
 			}
-			
-			scanner.close();
+		
 	
-}
+		}
+		
+		scanner.close();
 	}
 }
