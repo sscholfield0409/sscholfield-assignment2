0f08a321177cbae70ef88ce4296857432f7d4bf6
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-07-02 22:17:34 -0500

    Update Assignment2Application.java
    feeling like I'm so close!! Just have a minor bug I'm working on

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -12,23 +12,20 @@ public class Assignment2Application {
 		Random random = new Random();
 		
 		int randomNumber = random.nextInt(100) + 1;
-		int tries = 1;
-		
+		int tries = 2;
 		
 		System.out.println("Guess a number between 1 and 100");
 		int guessNumber1 = scanner.nextInt();
 	
-while (true) {
+while (true && tries <= 5) {
 	
-			if ( guessNumber1 < randomNumber && tries <= 5) {
+			if ( guessNumber1 < randomNumber) {
 				System.out.println("Please Pick a Higher Number" + ": ");
-			} else if ( guessNumber1 > randomNumber && tries <= 5) {
+			} else if ( guessNumber1 > randomNumber) {
 				System.out.println("Please Pick a Lower Number" + ": ");
 			}
 	
-			
 			guessNumber1 = scanner.nextInt();
-			
 			
 			if (guessNumber1 > 100 || guessNumber1 < 1) {
 				System.out.println("Your guess is not between 1 and 100, please try again");
@@ -41,11 +38,11 @@ while (true) {
 			if ( guessNumber1 != randomNumber && tries == 5) {
 				System.out.println("You Lose! The Number was: " + randomNumber);
 			}
-			
-	tries ++;
+		
+			tries ++;
 	
 		}
-		
+
 		scanner.close();
 	}
 	
