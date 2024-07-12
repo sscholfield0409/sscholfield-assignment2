ddd6566402e8362f34718f03ad94387a68215ab6
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-07-12 15:59:40 -0500

    Update Assignment2Application.java
    attempting to adjust scanner inputs which have altered the amount of attempts allowed

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -14,6 +14,7 @@ public class Assignment2Application {
 		int randomNumber = random.nextInt(100) + 1;
 		int tries = 2;
 		
+		
 		System.out.println("Guess a number between 1 and 100");
 		int guessNumber1 = scanner.nextInt();
 	
@@ -24,16 +25,15 @@ while (true && tries <= 5) {
 			} else if ( guessNumber1 > randomNumber) {
 				System.out.println("Please Pick a Lower Number" + ": ");
 			}
-	
-			guessNumber1 = scanner.nextInt();
 			
 			if (guessNumber1 > 100 || guessNumber1 < 1) {
 				System.out.println("Your guess is not between 1 and 100, please try again");
 				continue;
 			} else if (guessNumber1 == randomNumber && tries <= 5) {
-                System.out.println("You Win!! The number is: " + randomNumber);
-                break;
+		        System.out.println("You Win!! The number is: " + randomNumber);
+		        break;
 			}
+			
 			
 			if ( guessNumber1 != randomNumber && tries == 5) {
 				System.out.println("You Lose! The Number was: " + randomNumber);
