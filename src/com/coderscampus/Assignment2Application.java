34918e89065bf14e3a2c7bf98d6f39d8383fa8c4
sscholfield0409 <162228341+sscholfield0409@users.noreply.github.com>
2024-07-12 17:49:32 -0500

    Update Assignment2Application.java
    after some consulting, made some changes in the logic and flow of the code which resolved the issues I was having with invalid\valid attempts

--- src/com/coderscampus/Assignment2Application.java
+++ src/com/coderscampus/Assignment2Application.java
@@ -4,47 +4,41 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Assignment2Application {
-	
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner scanner = new Scanner(System.in);
 		Random random = new Random();
-		
+
 		int randomNumber = random.nextInt(100) + 1;
-		int tries = 2;
-		
-		
-		System.out.println("Guess a number between 1 and 100");
-		int guessNumber1 = scanner.nextInt();
-	
-while (true && tries <= 5) {
-	
-			if ( guessNumber1 < randomNumber) {
-				System.out.println("Please Pick a Higher Number" + ": ");
-			} else if ( guessNumber1 > randomNumber) {
-				System.out.println("Please Pick a Lower Number" + ": ");
-			}
-			
-			if (guessNumber1 > 100 || guessNumber1 < 1) {
+		int tries = 0;
+
+
+		while (tries < 5) {
+
+			System.out.println("Guess a number between 1 and 100");
+			int guess = scanner.nextInt();
+			if (guess > 100 || guess < 1) {
 				System.out.println("Your guess is not between 1 and 100, please try again");
 				continue;
-			} else if (guessNumber1 == randomNumber && tries <= 5) {
-		        System.out.println("You Win!! The number is: " + randomNumber);
-		        break;
+			} else if (guess < randomNumber) {
+				System.out.println("Please Pick a Higher Number" + ": ");
+			} else if (guess > randomNumber) {
+				System.out.println("Please Pick a Lower Number" + ": ");
+
+			} else if (guess == randomNumber) {
+				System.out.println("You Win!! The number is: " + randomNumber);
+				scanner.close();
+				return;
 			}
-			
-			
-			if ( guessNumber1 != randomNumber && tries == 5) {
-				System.out.println("You Lose! The Number was: " + randomNumber);
-			}
-		
-			tries ++;
-	
+			tries++;
+		}
+
+		if (tries == 5) {
+			System.out.println("You Lose! The Number was: " + randomNumber);
 		}
 
 		scanner.close();
 	}
-	
-	
+
 }
